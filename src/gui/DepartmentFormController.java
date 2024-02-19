ba4f5fdfb26dcab653825e57cec8e978a1d8a1f4
Herick Campos <herickzeno@gmail.com>
2024-02-19 11:06:32 -0300

    tratamento de excecoes e atualizacao da lista

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,9 +1,14 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
@@ -15,6 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
@@ -22,6 +28,8 @@ public class DepartmentFormController implements Initializable {
 	private Department entity;
 
 	private DepartmentService service;
+	
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
 
 	@FXML
 	private TextField txtId;
@@ -49,22 +57,41 @@ public class DepartmentFormController implements Initializable {
 		try {
 		entity = getFormDepartment();
 		service.saveOrUpdate(entity);
+		notifyDataChangeListener();
 		Utils.currentStage(event).close();
+		}catch ( ValidationException e) {
+			setErrorMenssages(e.getErrors());
 		}catch (DbException e) {
 			Alerts.showAlert("Error saving Object", "DBException", e.getMessage(), AlertType.ERROR);
 		}
 	}
 
+	private void notifyDataChangeListener() {
+			for(DataChangeListener listener: dataChangeListeners) {
+				listener.onDataChanged();
+			}
+	}
+
 	private Department getFormDepartment() {
 		Department dep = new Department();
+		
+		ValidationException exception = new ValidationException("Validation exception");
+		
 		dep.setId(Utils.tryParseToInt(txtId.getText()));
+		
+		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
+			exception.addError("name", "Fild cant empity");
+		}
 		dep.setName(txtName.getText());
+		
+		if (exception.getErrors().size() > 0 ) {
+			throw exception;
+		}
 		return dep;
 	}
 
 	@FXML
 	public void onBtCancelAction(ActionEvent event) {
-		System.out.println("cancel");
 		Utils.currentStage(event).close();
 	}
 
@@ -84,6 +111,10 @@ public class DepartmentFormController implements Initializable {
 	public void setDepartmentService(DepartmentService service) {
 		this.service = service;
 	}
+	
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener); 
+	}
 
 	public void updateFormData() {
 		if (entity == null) {
@@ -91,6 +122,14 @@ public class DepartmentFormController implements Initializable {
 		}
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
+	}
+	
+	private void setErrorMenssages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		
+		if (fields.contains("name")) {
+			lbError.setText(errors.get("name"));
+		}
 	}
 
 }
